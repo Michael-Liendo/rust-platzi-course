9afc21c135141ac79e246c8f5b42800e728ce718
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-05-04 11:25:52 -0400

    get and show user data from the terminal

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+fn get_and_show_user_data_in_the_terminal() {
+    println!("Please enter your name: ");
+    let mut name: String = String::new();
+    std::io::stdin().read_line(&mut name).unwrap();
+    name = name.trim().to_string();
+
+    println!("Please enter your age: ");
+    let mut age: String = String::new();
+    std::io::stdin().read_line(&mut age).unwrap();
+
+    let age: u32 = age.trim().parse().unwrap();
+
+    println!("Hello, {}! You have {} years old", name, age);
+}
+
 fn main() {
-    let name: &str = "Michael";
-    let year: u8 = 18;
-    println!("Hello, world! I'm {} have {} years...", name, year);
+    get_and_show_user_data_in_the_terminal()
 }
