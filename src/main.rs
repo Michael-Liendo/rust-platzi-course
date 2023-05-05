8c19a644b6c030866b4395d9f056f7a474479656
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-05-05 00:10:06 -0400

    conditionals

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+fn main() {
+    conditionals();
+
+    get_and_show_user_data_in_the_terminal();
+}
+
 fn get_and_show_user_data_in_the_terminal() {
     println!("Please enter your name: ");
     let mut name: String = String::new();
@@ -13,6 +19,16 @@ fn get_and_show_user_data_in_the_terminal() {
     println!("Hello, {}! You have {} years old", name, age);
 }
 
-fn main() {
-    get_and_show_user_data_in_the_terminal()
+fn conditionals() {
+    println!("Please enter your age: ");
+    let mut age: String = String::new();
+    std::io::stdin().read_line(&mut age).unwrap();
+
+    let age: u32 = age.trim().parse().unwrap();
+
+    if age >= 18 {
+        println!("You are an adult");
+    } else {
+        println!("You are a child");
+    }
 }
