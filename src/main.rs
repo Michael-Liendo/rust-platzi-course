59a02c42c68f33dcbf42e4e1724e4bedbb075cac
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-05-09 15:54:33 -0400

    some modules

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
-fn main() {
-    conditionals();
+use regex::Regex;
 
-    get_and_show_user_data_in_the_terminal();
+fn main() {
+    calculator();
 }
 
+fn calculator() {
+    let re_add: Regex = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
+
+    // get user input
+    println!("Please enter an operation: ");
+    let mut expression: String = String::new();
+    std::io::stdin().read_line(&mut expression).unwrap();
+
+    let caps = re_add.captures(expression.as_str()).unwrap();
+    let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+    let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+    let addition: i32 = left_value + right_value;
+
+    // show results
+    println!("Results: {}", addition)
+}
+
+#[allow(dead_code)]
 fn cicle_loop() {
     let mut counter = 0;
 
@@ -17,6 +36,7 @@ fn cicle_loop() {
     }
 }
 
+#[allow(dead_code)]
 fn get_and_show_user_data_in_the_terminal() {
     println!("Please enter your name: ");
     let mut name: String = String::new();
@@ -32,6 +52,7 @@ fn get_and_show_user_data_in_the_terminal() {
     println!("Hello, {}! You have {} years old", name, age);
 }
 
+#[allow(dead_code)]
 fn conditionals() {
     println!("Please enter your age: ");
     let mut age: String = String::new();
